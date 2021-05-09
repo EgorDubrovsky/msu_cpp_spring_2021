f5832b974eadafdad84adbf5ddc004c629bc2541
EgorDubrovsky <80921699+EgorDubrovsky@users.noreply.github.com>
2021-05-09 04:22:30 +0300

    Add files via upload
    Поменял табы на пробелы

--- 03/matrix.hpp
+++ 03/matrix.hpp
@@ -3,31 +3,31 @@
 
 class Matrix
 {
-	size_t rows;
-	size_t columns;
-	int32_t *data = nullptr;
-	
-	class ProxyRow
+    size_t rows;
+    size_t columns;
+    int32_t *data = nullptr;
+    
+    class ProxyRow
     {
         int32_t *data = nullptr;
-		size_t size;
+        size_t size;
     public:
         ProxyRow(int32_t*, size_t);
-		~ProxyRow();
+        ~ProxyRow();
         int32_t& operator[](size_t);
     };
 
 public:
-	Matrix(size_t, size_t);
-	Matrix(const Matrix&);
-	~Matrix();
-	Matrix &operator=(const Matrix&);
-	Matrix &operator*=(const int32_t);
-	ProxyRow operator[](size_t);
-	bool operator==(const Matrix&);
-	bool operator!=(const Matrix&);
-	Matrix operator+(const Matrix&);
-	friend std::ostream &operator<<(std::ostream&, const Matrix&);
-	size_t GetRows() const;
-	size_t GetColumns() const;
+    Matrix(size_t, size_t);
+    Matrix(const Matrix&);
+    ~Matrix();
+    Matrix &operator=(const Matrix&);
+    Matrix &operator*=(const int32_t);
+    ProxyRow operator[](size_t);
+    bool operator==(const Matrix&);
+    bool operator!=(const Matrix&);
+    Matrix operator+(const Matrix&);
+    friend std::ostream &operator<<(std::ostream&, const Matrix&);
+    size_t GetRows() const;
+    size_t GetColumns() const;
 };
